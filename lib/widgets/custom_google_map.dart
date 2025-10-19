4e7b445075b0f709621dfa3997552f6d885381e0
omar <omarabzaid358@gmail.com>
2025-10-19 15:18:14 +0300

    PolyLines

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -13,21 +13,23 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   late String nightMode;
+  late GoogleMapController googleMapController;
+
+  Set<Marker> markers = {};
+  Set<Polyline> polyLines = {};
 
   @override
   void initState() {
+    intMapPolyLines();
     initialCameraPosition = const CameraPosition(
       target: LatLng(31.198166190837064, 29.917136444493792),
-      zoom: 13,
+      zoom: 12,
     );
     intMapStyle();
+
     initMarker();
     super.initState();
   }
-
-  Set<Marker> markers = {};
-
-  late GoogleMapController googleMapController;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +37,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       alignment: AlignmentGeometry.bottomCenter,
       children: [
         GoogleMap(
+          polylines: polyLines,
           zoomControlsEnabled: false,
           markers: markers,
           //   mapType: MapType.satellite,
@@ -83,8 +86,25 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
         )
         .toSet();
     markers.addAll(myMarkers);
-    setState(() {
+    setState(() {});
+  }
 
-    });
+  void intMapPolyLines() {
+    var polyLine = Polyline(
+      //specify which line draw first
+      zIndex: 1,
+      //decide if the curve of the earth show or just a straight line
+      geodesic: true,
+      polylineId: PolylineId('1'),
+      points: [
+        LatLng(31.197361489921132, 29.904919533877298),
+        LatLng(31.209142831933246, 29.909801147072955),
+        LatLng(31.199674145961918, 29.91856661402628),
+        LatLng(31.209768411289144, 29.927106767373314),
+      ],
+      color: Colors.black,
+    );
+    polyLines.add(polyLine);
+    setState(() {});
   }
 }
