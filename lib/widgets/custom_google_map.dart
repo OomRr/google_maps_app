41d870267b4301ea6d5e21c7ddf100ba4375cd7a
omar <omarabzaid358@gmail.com>
2025-10-22 16:14:26 +0300

    updateMyLocation

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:mapsapp/models/placeModel.dart';
 
 class CustomGoogleMap extends StatefulWidget {
@@ -13,20 +14,21 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   late String nightMode;
-  late GoogleMapController googleMapController;
-
+  GoogleMapController? googleMapController;
+  Location location = Location();
   Set<Marker> markers = {};
   Set<Polyline> polyLines = {};
+  Set<Polygon> polygons = {};
 
   @override
   void initState() {
+    inquireAndEnableLocationService();
     intMapPolyLines();
+    intMapPolygon();
     initialCameraPosition = const CameraPosition(
       target: LatLng(31.198166190837064, 29.917136444493792),
       zoom: 12,
     );
-    intMapStyle();
-
     initMarker();
     super.initState();
   }
@@ -37,12 +39,16 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       alignment: AlignmentGeometry.bottomCenter,
       children: [
         GoogleMap(
-          polylines: polyLines,
+          //to add شكل مقفول
+          // polygons: polygons,
+          //شكل مفتوح
+          //   polylines: polyLines,
           zoomControlsEnabled: false,
           markers: markers,
           //   mapType: MapType.satellite,
           onMapCreated: (controller) async {
             googleMapController = controller;
+            //   intMapStyle();
           },
           initialCameraPosition: initialCameraPosition,
         ),
@@ -52,13 +58,23 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
           right: 16,
           bottom: 16,
           child: ElevatedButton(
-            onPressed: () {
+            onPressed: () async {
+              LocationData currentLocationData = await location.getLocation();
               //change location of the map
-              googleMapController.animateCamera(
+              googleMapController!.animateCamera(
                 CameraUpdate.newLatLng(
-                  LatLng(31.109060583785695, 30.120639261499885),
+                  LatLng(
+                    currentLocationData.latitude!,
+                    currentLocationData.longitude!,
+                  ),
                 ),
               );
+              Marker myLocationMarker = Marker(
+                markerId: MarkerId('myLocation'),
+                position: LatLng(currentLocationData.latitude!, currentLocationData.longitude!),
+              );
+              markers.add(myLocationMarker);
+              setState(() {});
             },
             child: const Text('click me'),
           ),
@@ -71,7 +87,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     nightMode = await rootBundle.loadString(
       'assets/googleMapsStyles/night_style.json',
     );
-    googleMapController.setMapStyle(nightMode);
+    googleMapController!.setMapStyle(nightMode);
   }
 
   void initMarker() {
@@ -85,7 +101,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
           ),
         )
         .toSet();
-    markers.addAll(myMarkers);
+    //markers.addAll(myMarkers);
     setState(() {});
   }
 
@@ -105,6 +121,79 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       color: Colors.black,
     );
     polyLines.add(polyLine);
-    setState(() {});
+  }
+
+  void intMapPolygon() {
+    var polygon = Polygon(
+      fillColor: Colors.black38,
+      strokeWidth: 0,
+      //specify which line draw first
+      zIndex: 1,
+      //decide if the curve of the earth show or just a straight line
+      geodesic: true,
+      polygonId: PolygonId('1'),
+      points: [
+        LatLng(31.197361489921132, 29.904919533877298),
+        LatLng(31.209142831933246, 29.909801147072955),
+        LatLng(31.199674145961918, 29.91856661402628),
+        LatLng(31.209768411289144, 29.927106767373314),
+      ],
+    );
+    polygons.add(polygon);
+  }
+
+  Future<void> inquireAndEnableLocationService() async {
+    var isLocationEnabled = await location.serviceEnabled();
+    if (!isLocationEnabled) {
+      isLocationEnabled = await location.requestService();
+    }
+    if (!isLocationEnabled) {
+      ///Todo:pop-up error
+    }
+    inquireAndEnableLocationPermission();
+  }
+
+  Future<bool> inquireAndEnableLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+
+      ///Todo:pop-up error
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+    }
+
+    if (permissionStatus != PermissionStatus.granted) {
+      return false;
+
+      ///Todo:pop-up error
+    }
+
+    return true;
+  }
+
+  void getCurrentLocation() async {
+location.changeSettings(distanceFilter: 1);
+    location.onLocationChanged.listen((event) {
+      googleMapController?.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(event.latitude!, event.longitude!)),
+        ),
+      );
+      Marker myLocationMarker = Marker(
+        markerId: MarkerId('myLocation'),
+        position: LatLng(event.latitude!, event.longitude!),
+
+      );
+      markers.add(myLocationMarker);
+      setState(() {});
+    });
+  }
+
+  Future<void> updateMyLocation() async {
+    await inquireAndEnableLocationService();
+    await inquireAndEnableLocationPermission();
+    getCurrentLocation();
   }
 }
